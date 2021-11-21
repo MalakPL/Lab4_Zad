1e44455cad872954b1c0a5da8180ae6b1c485c35
Malak <szmajdakarol@gmail.com>
2021-11-21 12:24:20 +0100

    Fixy

--- Lab4_Zad/Lab4_Zad.cpp
+++ Lab4_Zad/Lab4_Zad.cpp
@@ -16,6 +16,10 @@ using std::setw;
 
 const char* IntCastExceptionMessage = "Podana wartosc nie mogla zostac zrzutowana do typu int.";
 
+/*
+Znajdź największy element danej tablicy liczb całkowitych.
+Liczbę elementów tablicy i ich wartości pobierz od użytkownika.
+*/
 auto Zadanie1() -> void
 {
 	wcout << "[Zadanie 1]" << endl;
@@ -47,10 +51,17 @@ auto Zadanie1() -> void
 	}
 }
 
+
+/*
+Napisz program, który wskazuje najczęściej występujący element
+w tablicy liczb całkowitych. Liczbę elementów tablicy i ich wartości
+pobierz od użytkownika.
+*/
 auto Zadanie2() -> void
 {
 	wcout << "[Zadanie 2]" << endl;
 
+#pragma region Liczbę elementów tablicy i ich wartości pobierz od użytkownika.
 	int N;
 	wcout << L"Podaj rozmiar tablicy: "; cin >> N;
 
@@ -62,16 +73,18 @@ auto Zadanie2() -> void
 	}
 
 	int* Tablica = new int[N];
-	map<int, int> FreqMap;
+	map<int, int> FreqMap; /* Słowniczek posłuży do liczenia występowań elementów w tablicy */
 
 	for (int i = 0; i < N; ++i)
 	{
 		wcout << L"Podaj wartość dla Tablica[" << i << "]: "; cin >> Tablica[i];
 		if (cin.fail()) { throw exception{ IntCastExceptionMessage }; }
 
-		++FreqMap[Tablica[i]];
+		++FreqMap[Tablica[i]]; //Inkrementacja częstotliwośći występowania danego elementu
 	}
+#pragma endregion
 
+#pragma region Najczęściej występujący element w tablicy liczb całkowitych.
 	int Max = 0;
 	int Number = 0;
 	for (auto const& i : FreqMap)
@@ -83,6 +96,7 @@ auto Zadanie2() -> void
 		}
 	}
 	wcout << L"Najczęściej występujący element tablicy to: " << Number << L" (" << Max << L" występowań)" << endl << endl;
+#pragma endregion
 
 	if (Tablica != nullptr)
 	{
@@ -91,40 +105,54 @@ auto Zadanie2() -> void
 	}
 }
 
+
+/*
+Napisz program, który w zadeklarowanej tablicy dwuwymiarowej 10x10 umieszcza:
+- w pierwszej kolumnie liczby od 0 do 9;
+- w drugiej sumę tych liczb (np. 0+0, 1+1 itd..);
+(1 p.)
+* Część bonusowa:
+- w trzeciej – kwadraty tych liczb;
+- w czwartej – sumę numeru wiersza i numeru kolumny;
+- w piątej – wynik różnicy numeru wiersza i numeru kolumny;
+- w pozostałych kolumnach 0 (interpretacja graficzna tablicy poniżej).
+(*2 p.)
+*/
 auto Zadanie3() -> void
 {
 	wcout << "[Zadanie 3]" << endl;
 
-	int Tablica[10][10]{};
+	const int N = 10;
+	int Tablica[N][N]{};
 
-	for (int y = 0; y < 10; ++y)
+	for (int y = 0; y < N; ++y)
 	{
-		for (int x = 0; x < 10; ++x)
+		for (int x = 0; x < N; ++x)
 		{
 			switch (y)
 			{
 			case 0:
-				Tablica[y][x] = x;
+				Tablica[y][x] = x; //- w pierwszej kolumnie liczby od 0 do 9;
 				break;
 
 			case 1:
-				Tablica[y][x] = (x << 1);
+				Tablica[y][x] = (x << 1); //- w drugiej sumę tych liczb (np. 0+0, 1+1 itd..); X+X <=> 2X <=> przesuniecie bitowe w lewo (szybsze)
 				break;
 
-			case 2:
-				Tablica[y][x] = x * x;
+			case 2: 
+				Tablica[y][x] = x * x; //- w trzeciej – kwadraty tych liczb;
 				break;
 
 			case 3:
-				Tablica[y][x] = (x + 1) + (y + 1);
+				Tablica[y][x] = (x + 1) + (y + 1); //- w czwartej – sumę numeru wiersza i numeru kolumny; (numer, nie indeks, czyli numeracja od 1...N)
 				break;
 
 			case 4:
-				Tablica[y][x] = (x + 1) - (y + 1);
+				Tablica[y][x] = (x + 1) - (y + 1); //- w piątej – wynik różnicy numeru wiersza i numeru kolumny; (numer, nie indeks, czyli numeracja od 1...N)
 				break;
 
 			default:
-				Tablica[y][x] = 0;
+				Tablica[y][x] = 0; //- w pozostałych kolumnach 0
 				break;
 			}
 
